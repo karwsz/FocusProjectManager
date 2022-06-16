13328ea2fb223a70c631027a3ee64451d3247511
KarwszPL <karwszpl@gmail.com>
2022-06-16 14:40:57 +0200

    Initial commit

---
+++ src/main/java/pl/karwsz/FocusProjectManager/FocusProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.karwsz.FocusProjectManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusProjectManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/karwsz/FocusProjectManager/FocusProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.karwsz.FocusProjectManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
