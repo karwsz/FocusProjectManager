e8aea14ceeb4129dfd4688ef394ff7f7d94658ac
KarwszPL <karwszpl@gmail.com>
2022-06-16 23:21:48 +0200

    Changed to GET, getMapping -> main

--- src/main/java/pl/karwsz/FocusProjectManager/controller/MainController.java
+++ src/main/java/pl/karwsz/FocusProjectManager/controller/MainController.java
@@ -1,14 +1,15 @@
 package pl.karwsz.FocusProjectManager.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-    @RequestMapping
-    public String getMapping() {
+    @GetMapping
+    public String main() {
         return "Mapping";
     }
 
